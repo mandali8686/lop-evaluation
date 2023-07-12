7a5d0eac5170e2f780a814de0157f848f11c7d90
manda <manda.li8686@gmail.com>
2023-07-12 16:44:01 -0500

    added connection in Methodology

--- src/components/Methodology.js
+++ src/components/Methodology.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './Methodology.css';
+import { useNavigate } from 'react-router-dom';
+
 
 
 function Methodology() {
+  const navigate = useNavigate();
   return (
     <div className='methodology'>
       <h2>Methodology</h2>
@@ -26,6 +29,9 @@ function Methodology() {
       <a href='https://drive.google.com/file/d/1tPAKjEo2QLOpS9G2Bw39lzet5sKS5WlI/view?usp=drive_link'> 2023 LOP Alumni Survery Literature Review</a>
         <br></br>
         <br></br>
+        <div className='go'>
+                    <button onClick={() => navigate('/raw-data')}>Check All the Raw Data </button>
+                </div>
         
       <h4><i>2012 proposed program evaluation methodology</i></h4>
       <a href='https://drive.google.com/file/d/1Jnhs-zcnNwnSppjfVFm1PQ9Mn5fHY4Tx/view?usp=drive_link'>2012 Evaluation Report</a>
@@ -37,6 +43,9 @@ function Methodology() {
         <h3>Competitive Analysis</h3>
         <a href='https://docs.google.com/document/d/13x1qz1Xd6qSeuT3dl83goOFtzkdNW8H4/edit?usp=sharing&ouid=110580296038753455992&rtpof=true&sd=true'>Research on Comparable Programs</a>
         <br></br>
+        <br></br>
+        <a href='https://docs.google.com/spreadsheets/d/1pbSfvdi989SFKpSqV2WEuzeyhn4Ymksl/edit?usp=sharing&ouid=110580296038753455992&rtpof=true&sd=true'>Competitors and Requirement</a>
+
 
       </div>
 <div className='focus-group'>
