5192085317d821426d7388241402873fd430e3e9
manda <manda.li8686@gmail.com>
2023-07-12 14:11:21 -0500

    updated Executive summary and competitor

--- src/components/Methodology.js
+++ src/components/Methodology.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Methodology.css';
 
+
 function Methodology() {
   return (
     <div className='methodology'>
       <h2>Methodology</h2>
+      <div className='survey-container'>
+      <div className='survey-links'>
+        <br></br>
+        <h3>LOP/MLO Survey</h3>
       <br></br>
       <a href="https://forms.office.com/Pages/DesignPageV2.aspx?subpage=design&id=OX9aur7js0q0UGf6gPrsrYsr_kqE6MBEjWNLC-YocW9UQk44WVBWVlI3WEw1MElFTk1FT0MyUThPTyQlQCN0PWcu" >LOP Alumni Suvery</a>
       <br></br>
@@ -18,6 +23,22 @@ function Methodology() {
       <a href="https://forms.office.com/Pages/DesignPageV2.aspx?subpage=design&id=OX9aur7js0q0UGf6gPrsrS51YCzhKJtJsbfx3NYXkkBUMUdESktCNlJYVzZQV0NGWEpaRVpLNEQwSS4u" >MLO Graduating Student Suvery</a>
       <br></br>
       <br></br>
+      </div>
+      <div className='literature'>
+        <br></br>
+        <h3>LOP Literature Review</h3>
+        <a href='https://drive.google.com/file/d/1tPAKjEo2QLOpS9G2Bw39lzet5sKS5WlI/view?usp=drive_link'> 2023 LOP Alumni Survery Literature Review</a>
+        <br></br>
+        <br></br>
+        <a href='https://docs.google.com/document/d/13x1qz1Xd6qSeuT3dl83goOFtzkdNW8H4/edit?usp=sharing&ouid=110580296038753455992&rtpof=true&sd=true'>Research on Comparable Programs</a>
+        <br></br>
+
+      </div>
+
+      </div>
+      <br></br>
+      <h3>LOP Logic Model</h3>
+      <img className="big-graph" src={`${process.env.PUBLIC_URL}/LogicModel_LOP.png`} alt="Graph 2" />
     </div>
   );
 }
--- src/components/raw-data.js
+++ src/components/raw-data.js
@@ -37,9 +37,12 @@ function RawData() {
                 <h3>Survey Raw Data</h3>
                 <a href='https://docs.google.com/spreadsheets/d/1bzNVk7ca-5c8XrjDX4s-gHAaLcso_sAma-f5dhkKJzc/edit?usp=sharing'>LOP Alumni Raw Data</a>
                 <br></br>
+                <br></br>
                 <a href='https://docs.google.com/spreadsheets/d/1FpOZy0e4WUqMQYkzcwQPBK3lAmzDhV0CMzBimO0hZgs/edit?usp=sharing'>LOP Graduating Student Raw Data</a>
                 <br></br>
+                <br></br>
                 <a href='https://docs.google.com/spreadsheets/d/1vizcGpq9W6Dy98_tHkkdx5InQi_OQ9i6ZuzocX40rOE/edit?usp=sharing'>MLO Graduating Student Raw Data</a>
+                <br></br>
                 <br></br>
                 <a href='https://docs.google.com/spreadsheets/d/1fMxO5-mj9dSpZOOcBN7fGI07Zmzdk5SKtEZXbjUQljI/edit?usp=sharing'>LOP Current Student Raw Data</a>
             </div>
@@ -47,16 +50,30 @@ function RawData() {
                 <h3>Focus Group Data Links</h3>
                 <a href='https://docs.google.com/document/d/1FXZnWE9Mkp0biGb0mJ7VSfRYp6OXgIjW/edit?usp=sharing&ouid=110580296038753455992&rtpof=true&sd=true'>Focus Group 1 Raw Data</a>
                 <br></br>
+                <br></br>
                 <a href='https://docs.google.com/document/d/1EnX8nrFYpMF6p3Fn7vZ3HASwLbR3IyPM/edit?usp=sharing&ouid=110580296038753455992&rtpof=true&sd=true'> Focus Group 2 Raw Data</a>
+                <br></br>
                 <br></br>
                 <a href='https://docs.google.com/document/d/11t_pRNz7QAixc80piGGSM5iRrkyT_Gam/edit?usp=sharing&ouid=110580296038753455992&rtpof=true&sd=true'> Focus Group 3 Raw Data</a>
                 <br></br>
+                <br></br>
                 <a href='https://docs.google.com/document/d/1GHxfLEQWcE6xpGHFbPyY1hnHWT7V_Jb-/edit?usp=sharing&ouid=110580296038753455992&rtpof=true&sd=true'>Focus Group 4 Raw Data</a>
+                <br></br>
                 <br></br>
                 <a href='https://docs.google.com/document/d/10Dm5NrqbJ7Zwn02cHWB5oFuMYm37m3zf/edit?usp=sharing&ouid=110580296038753455992&rtpof=true&sd=true'>Focus Group 5 Raw Data</a>
 
             </div>
+            <div className='Competitors'>
+                <h3>Competitors</h3>
+                <a href='https://docs.google.com/spreadsheets/d/1yeOua1Wby5PjugI6paL9R6okGivuvgLZVoE7302dHjA/edit?usp=sharing'>Competitors and Core Courses</a>
+                <br></br>
+                <br></br>
+                <a href='https://docs.google.com/spreadsheets/d/1pbSfvdi989SFKpSqV2WEuzeyhn4Ymksl/edit?usp=sharing&ouid=110580296038753455992&rtpof=true&sd=true'>Competitors and Requirement</a>
+
             </div>
+            </div>
+            <br></br>
+            <br></br>
             
             {data.map((csvData, index) => (
                 <div key={index}>
